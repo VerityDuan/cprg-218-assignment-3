3f37e8c0fa926fcc9029b9e8a81c059a5afdde85
VerityDuan <verityduan@gmail.com>
2025-04-13 00:38:47 -0600

    weather api

---
+++ weather.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const weatherDiv = document.getElementById('weather');
+  weatherDiv.innerHTML = `
+      <h2>Weather in Cancun</h2>
+      <p>🌡️ Temperature: 18°C</p>
+      <p>☁️ Conditions: clear sky</p>
+  `;
+});
+
+
